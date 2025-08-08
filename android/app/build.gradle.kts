56de155eab8fefce53228f494ce32ef11d9580ad
Hrd <hardipublic@gmail.com>
2025-08-08 09:30:19 +0700

    Ganti ndk versi

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.ppkd_b_3"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "29.0.13113456"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
